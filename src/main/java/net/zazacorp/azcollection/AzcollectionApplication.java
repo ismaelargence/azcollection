d01399147f42fc2ebd0a6f4b97bf659c597d4cd8
Matheo Nguyen <matheo.ngn@gmail.com>
2023-04-25 22:05:25 +0200

    Version clean

--- src/main/java/net/zazacorp/azcollection/AzcollectionApplication.java
+++ src/main/java/net/zazacorp/azcollection/AzcollectionApplication.java
@@ -2,13 +2,20 @@ package net.zazacorp.azcollection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AzcollectionApplication {
 
 	public static void main(String[] args) {
-		System.out.println("oui");
 		SpringApplication.run(AzcollectionApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String greet(){
+		return "Hello";
 	}
 
 }
